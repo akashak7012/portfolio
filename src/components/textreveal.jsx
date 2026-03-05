24ca7b2251d4cd7787288fa28751cf8b841f1c86
Akash <akashak7012@gmail.com>
2026-03-05 09:43:09 +0530

    Delete src directory

--- src/components/textreveal.jsx
+++
@@ -1,5 +0,0 @@
-import { TextReveal } from "@/components/magicui/text-reveal";
-
-export default function TextRevealDemo() {
-  return <TextReveal>Portfolio That's Cooler than my CGPA </TextReveal>;
-}
